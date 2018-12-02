deb090d8c069e056bd5aa38ddb077ec9be7009ea
Bjørnar Snoksrud <bsnoksru@cisco.com>
2018-12-02 12:54:10 +0100

    day1, pt2

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,15 +1,24 @@
 module Day1 where
 
-import Data.Foldable
 import Data.Maybe
-import Data.Monoid
+import Data.IntSet as I
 import Text.Read
 
 parse :: String -> Maybe Int
 parse ('+': num) = readMaybe num
 parse num = readMaybe num
 
+dup xs = go xs (I.singleton 0) 0
+  where
+    go (x: xs) set acc =
+      let newAcc = acc + x in
+      case newAcc `I.member` set of
+        True -> newAcc
+        _ -> go xs (newAcc `I.insert` set) newAcc
+
 day1 :: IO ()
 day1 = do
   i <- lines <$> readFile "data/day1.txt"
-  print $ sum (catMaybes (parse <$> i))
+  let nums = catMaybes (parse <$> i)
+  print $ sum nums
+  print $ dup (cycle nums)
