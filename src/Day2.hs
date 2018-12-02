74455da2daab1ca8c9bb2f90c728d1ba4d02d77b
Bjørnar Snoksrud <bsnoksru@cisco.com>
2018-12-02 13:30:46 +0100

    day2, pt2

--- src/Day2.hs
+++ src/Day2.hs
@@ -9,8 +9,25 @@ checksums f =
       has = Sum . fromEnum . (`elem` counts)
   in (has 2, has 3)
 
+dropLetter :: Int -> [a] -> [a]
+dropLetter idx xs = let
+  tail' (x:xs) = xs
+  tail' _ = []
+  (heads, tails) = splitAt idx xs
+  in heads <> tail' tails
+
+offByOneFor :: Eq a => [[a]] -> Int -> [[a]]
+offByOneFor xs n = let xs' = dropLetter n <$> xs in
+ xs' \\ (nub xs')
+
+offByOne :: Eq a => [[a]] -> [[a]]
+offByOne xs = let
+  len = length $ head xs
+  in concatMap (offByOneFor xs) [0..len]
+
 day2 :: IO ()
 day2 = do
   i <- lines <$> readFile "data/day2.txt"
   let (Sum doubles, Sum triples) = mconcat $ checksums <$> i
   print (doubles * triples)
+  print (intercalate ", " $ offByOne i)
