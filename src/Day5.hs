1725477edb68d6b201ff8c7a445856950d6c7d9f
Bjørnar Snoksrud <bsnoksru@cisco.com>
2018-12-05 08:34:43 +0100

    day5, cleanup

--- src/Day5.hs
+++ src/Day5.hs
@@ -20,12 +20,15 @@ recWhile f xs = let (xs', repeat) = f xs
   then recWhile f xs'
   else xs'
 
+react' = recWhile react
+
 day5 :: IO ()
 day5 = do
   unless (reactive 'a' 'A' && reactive 'A' 'a') $ putStrLn "ERROR"
   when (reactive 'a' 'a' || reactive 'A' 'A' || reactive 'a' 'b') $ putStrLn "ERROR"
+
   input <- filter isLetter <$> readFile "data/day5.txt"
-  putStrLn ("Part1: " <> show (length $ recWhile react input))
-  let alternatives = (\x -> filter ((/= x).toLower) input) <$> ['a'..'z']
-  let best = minimum (length . recWhile react <$> alternatives)
+  putStrLn ("Part1: " <> (show . length . react' $ input))
+  let remove xs x = filter ((/= x).toLower) xs
+  let best = minimum $ length . react' . remove input <$> ['a'..'z']
   putStrLn ("Part2: " <> show best)
