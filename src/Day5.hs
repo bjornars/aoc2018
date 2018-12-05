2f9905b4ee2abe0616f3e7ef2494804d25bf24fb
Bjørnar Snoksrud <bsnoksru@cisco.com>
2018-12-05 15:15:49 +0100

    day5: single-pass

--- src/Day5.hs
+++ src/Day5.hs
@@ -8,19 +8,13 @@ reactive x y | isUpper x == isUpper y = False
 reactive x y | isUpper x = reactive y x
 reactive x y = y == toUpper x && not (isUpper x)
 
-react :: String -> (String, Bool)
-react  str = go str [] False
+react :: String -> String
+react  str = go str []
   where
-    go (x : y : xs) acc _ | x `reactive` y = go xs acc True
-    go (x : xs) acc changed = go xs (x : acc) changed
-    go [] acc changed  = (reverse acc, changed)
-
-recWhile f xs = let (xs', repeat) = f xs
-  in if repeat
-  then recWhile f xs'
-  else xs'
-
-react' = recWhile react
+    go (x : y : xs) (a : acc)  | x `reactive` y = go (a:xs) acc
+    go (x : y : xs) []  | x `reactive` y = go xs []
+    go (x : xs) acc = go xs (x : acc)
+    go [] acc  = acc
 
 day5 :: IO ()
 day5 = do
@@ -28,7 +22,8 @@ day5 = do
   when (reactive 'a' 'a' || reactive 'A' 'A' || reactive 'a' 'b') $ putStrLn "ERROR"
 
   input <- filter isLetter <$> readFile "data/day5.txt"
-  putStrLn ("Part1: " <> (show . length . react' $ input))
+  putStrLn ("Part1: " <> (show . length . react $ input))
+
   let remove xs x = filter ((/= x).toLower) xs
-  let best = minimum $ length . react' . remove input <$> ['a'..'z']
+  let best = minimum $ length . react . remove input <$> ['a'..'z']
   putStrLn ("Part2: " <> show best)
