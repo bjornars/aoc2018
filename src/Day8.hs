8b199ac955671dcb84cfc66b9d134db628ce4f72
Bjørnar Snoksrud <bsnoksru@cisco.com>
2018-12-10 10:49:54 +0100

    day8: golf

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Day8 where
 
-import Control.Monad.State
-import Data.Monoid
+import Control.Arrow
+import Control.Monad
+import Debug.Trace
+import Data.Char
 
-import Text.Read hiding (lift, get)
+import Text.ParserCombinators.ReadP
 
 data Node a = Node [Node a] [a] deriving (Show)
 
@@ -22,45 +25,26 @@ value (Node ns xs) = sum (value <$> nodes)
   where count xs a = length $ filter (==a) xs
         nodes = concat $ zipWith replicate (count xs <$> [1..]) ns
 
-type ParseError = String
-type Parser i o = StateT [i] (Either ParseError) o
-
-
-err :: String -> Parser a b
-err str = lift $ Left str
-
-eat :: Int -> Parser i [i]
-eat n = do
-  input <- get
-  when (n > length input) $ err "ran out of input"
-  let (xs, rest) = splitAt n input
-  put rest
-  return xs
-
-one :: Parser i i
-one = head <$> eat 1
-
-parseNode :: Parser Int (Node Int)
+parseNode :: ReadP (Node Int)
 parseNode = do
-  n_children <- one
-  n_meta <- one
-  children <- replicateM n_children parseNode
-  metadata <- eat n_meta
-  return $ Node children metadata
+   n_children <- parseDigit
+   n_meta <- parseDigit
+   Node
+     <$> replicateM n_children parseNode
+     <*> replicateM n_meta parseDigit
+    where parseDigit = char ' ' >> read @Int <$> many1 (satisfy isDigit)
 
-
-runParser :: Parser Int (Node Int) -> [Int] -> Either ParseError (Node Int)
-runParser parser input = do
-  (result, remainingState) <- runStateT parser input
-  if null remainingState then Right result
-  else Left "too much input"
+runParser :: ReadP (Node Int) -> String -> Either String (Node Int)
+runParser parser = readP_to_S (parser) >>> \case
+  [(res, _)] -> Right res
+  [] -> Left "no parse"
 
 note :: a -> Maybe b -> Either a b
 note msg = maybe (Left msg) Right
 
 day8 = do
-  input <- note "couldnt parse file" . traverse (readMaybe @Int) . words <$> readFile "data/day8.txt"
-  let tree = input >>= runParser parseNode
+  input <- readFile "data/day8.txt"
+  let tree = runParser parseNode (' ': input)
   case tree of
     Right n -> do
       putStrLn $ "Part 1: " <> show (sum n)
